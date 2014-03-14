20c6836d4e946bbc00e1fe70fe96631f306416de
Matthew Gorski <mgorski8@gmail.com>
2014-03-14 12:09:28 -0400

    Adds the skeleton for the board class

--- src/board.py
+++ src/board.py
@@ -1,3 +1,67 @@
 '''
 This module defines a class for representing the board in a game of tic-tac-toe.
 '''
+
+
+class Board(object):
+    '''
+    Instances of this class represent a board for playing tic-tac-toe.
+
+    *Constructor arguments: none
+    '''
+    # Locations on the board are indexed as follows:
+    # 0 | 1 | 2
+    #--------------
+    # 3 | 4 | 5
+    #--------------
+    # 6 | 7 | 8
+    winners = (['x','x','x'], ['o','o','o'])
+
+    def __init__(self):
+        pass
+
+    def place(self, index, play):
+        '''
+        Places the play on the board at the index.
+
+        *Arguments
+        index: an integer 0 through 8
+        play: the strings "x" or "o"
+
+        *Raises: IndexError if index is already occupied
+        '''
+        pass
+
+    def result(self):
+        '''
+        *Returns:
+        "x" or "o" if "x" or "o" has won
+        "Tie" if the game is over and neither has won
+        "None" if the game is not over
+        '''
+        pass
+
+    @property
+    def rows(self)
+        '''
+        Gets a list of the rows on the board.
+        '''
+        pass
+
+    @property
+    def columns(self):
+        '''
+        Gets a list of the columns on the board.
+        '''
+        pass
+
+    @property
+    def diagonals(self):
+        '''
+        Gets a list of the diagonals on the board (left to right first,
+        right to left second).
+        '''
+        pass
+
+    def __str__(self):
+        pass
