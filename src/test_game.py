d532fe3e9cf4fd7c2d28172b0a3314dd527fb25e
Matthew Gorski <mgorski8@gmail.com>
2014-03-19 13:31:43 -0400

    Adds tests for game class

--- src/test_game.py
+++ src/test_game.py
@@ -9,4 +9,19 @@ from game import Game
 
 
 class TestGame(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.game1 = Game(random_strat, random_strat, "A", "B")
+
+    def test_game(self):
+
+        self.game1.play_game()
+        result = self.game1.result
+        self.assertIn(result, ("Tie", "x", "o"))
+        if result == "Tie":
+            self.assertEqual(self.game1.winner, "No one")
+        else:
+            self.assertIn(self.game1.winner, ("A", "B"))
+        self.assertRaises(ValueError, self.game1.play_game)
+
+        
