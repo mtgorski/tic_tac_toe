df8e1c3197f34ae510cb055c30e36d9e91d401af
Matthew Gorski <mgorski8@gmail.com>
2014-03-25 14:42:29 -0400

    Moves user input function to its own module because it is going to be used elsewhere than in main.

--- src/main.py
+++ src/main.py
@@ -5,27 +5,7 @@ This module contains the main program logic.
 '''
 import strategies
 import game
-
-
-def get_input(prompt, options=None):
-    '''
-    Asks the user for input, using the prompt. If options are specified,
-    repeats the question until the answer is in the options.
-
-    *Arguments
-    prompt (str): the phrase displayed to the user when asking for input
-    options (collection): container of valid inputs as strings
-    
-    '''
-    while True:
-        answer = raw_input(prompt+"  ")
-        if options and answer not in options:
-            print "That's not a valid input."
-            print "Valid options are: "+", ".join(options)+".\n"
-        else:
-            break
-    return answer
-
+import user_input
 
 def main():
     strategies.human.__name__ = "Human"
@@ -35,7 +15,7 @@ def main():
     print "Let's play tic-tac-toe!\n"
     again = "y"
     while again == "y":
-        go_first = get_input("Would you like to go first (y or n)?", ("y", "n"))
+        go_first = user_input.get_input("Would you like to go first (y or n)?", ("y", "n"))
         print "\n"
         if go_first == "y":
             first = strategies.human
@@ -45,7 +25,7 @@ def main():
             second = strategies.human
         g = game.Game(first, second)
         g.play_game(display=True)
-        again = get_input("Would you like to play again (y or n)?", ("y", "n"))
+        again = user_input.get_input("Would you like to play again (y or n)?", ("y", "n"))
 
 
 if __name__ == "__main__":
