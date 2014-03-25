f19129a77306baba65a3fabcf64668e9f77f5cc9
Matthew Gorski <mgorski8@gmail.com>
2014-03-25 14:20:31 -0400

    Adds function for getting user input.

--- src/main.py
+++ src/main.py
@@ -3,3 +3,22 @@ Python 2.7
 
 This module contains the main program logic.
 '''
+
+
+def get_input(prompt, options=None):
+    '''
+    Asks the user for input, using the prompt. If options are specified,
+    repeats the question until the answer is in the options.
+
+    *Arguments
+    prompt (str): the phrase displayed to the user when asking for input
+    
+    '''
+    while True:
+        answer = raw_input(prompt+"  ")
+        if options and answer not in options:
+            print "That's not a valid input."
+            print "Valid options are: "+", ".join(options)+".\n"
+        else:
+            break
+    return answer
