b83a0c7d5cd592b7df6e44709af2f3144f3279d0
Matthew Gorski <mgorski8@gmail.com>
2014-06-09 12:04:35 -0400

    Adds ability for play view to return context for first request

--- src/ttt_app/tests/test_views.py
+++ src/ttt_app/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test.utils import setup_test_environment
 from django.test import TestCase, Client
+from django.http import HttpResponseNotAllowed
 
 import os, sys, inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -7,6 +8,8 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
 from board import Board
+from strategies import perfect
+from views import construct_board
 
 
 class PlayFunction(TestCase):
@@ -16,8 +19,48 @@ class PlayFunction(TestCase):
         self.url = '/play'
         setup_test_environment()
 
-    def test_getRequestUsesBoardTemplate(self):
-        response = self.client.get(self.url)
+    def test_postRequestUsesBoardTemplate(self):
+        post_data = {'player_first':'true'}
+        response = self.client.post(self.url, post_data)
         template = 'ttt_app/board.html'
         self.assertTemplateUsed(response, template)
 
+    def test_getRequestRaises405(self):
+        response = self.client.get(self.url)
+        self.assertIsInstance(response, HttpResponseNotAllowed)
+
+    def test_playerFirstWithtNoBoardReturnsResponseContextWithEmptyBoard(self):
+        post_data = {'player_first':'true'}
+        response = self.client.post(self.url, post_data)
+        expected = Board()
+        actual = response.context['board_object']
+        self.assertEqual(expected, actual)
+
+    def test_AIFirstWithNoBoardReturnsResponseContextWithOneMove(self):
+        post_data = {'player_first':'false'}
+        response = self.client.post(self.url, post_data)
+        board = Board()
+        ai_move = board.place(*perfect(board))
+        actual = response.context['board_object']
+        self.assertEqual(actual, board)
+
+    
+
+
+class ConstructBoardFunction(TestCase):
+
+    def setUp(self):
+        self.post_dict = dict(('board{}'.format(n), '-') for n in range(9))
+
+    def test_normalUse1(self):
+        result = construct_board(self.post_dict)
+        expected = Board()
+        self.assertEqual(result, expected)
+
+    def test_normalUse2(self):
+        self.post_dict['board0'] = 'x'
+        result = construct_board(self.post_dict)
+        init_board = range(9)
+        init_board[0] = 'x'
+        expected = Board(init_board)
+        self.assertEqual(expected, result)
--- src/ttt_app/views.py
+++ src/ttt_app/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.template import RequestContext
 
 from board import Board
+from strategies import perfect
 
 # Create your views here.
 def play(request):
-    context = {}
+    '''
+    The view handles the playing of a game of tic-tac-toe. Expects a post
+    request.
+        -Post must have the key 'player_first' with value 'true' or 'false'
+        -Post can have keys 'board<n>' for n in range(9) with values 'x', 'o' or '-'
+    '''
+    # Only allow post requests b/c we always need info about the game
+    # being played
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+    if request.POST['player_first'] == 'true':
+        board = Board()
+    else:
+        board = Board()
+        board.place(*perfect(board))
+    context = {'board_object':board}
     return render(request, 'ttt_app/board.html', context)
+
+
+def construct_board(post_dict):
+    '''
+    Constructs a tic-tac-toe board from a dictionary representing a post request.
+
+    *Returns: Board object
+    *Raises: MultiKeyValueError if the post is not sufficient to describe the board
+    '''
+    board = range(9)
+    for i in range(9):
+        key = 'board{}'.format(i)
+        value = post_dict[key]
+        if value in ('x', 'o'):
+            board[i] = value
+    return Board(board)
