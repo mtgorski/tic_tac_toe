43aa7afe4a3aba826109200552616860c049eaca
Matthew Gorski <mgorski8@gmail.com>
2014-06-16 14:41:17 -0400

    Adds route

--- src/ttt_project/urls.py
+++ src/ttt_project/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ttt_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('ttt_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^3T/', include('ttt_app.urls', namespace='3T'))
 )
